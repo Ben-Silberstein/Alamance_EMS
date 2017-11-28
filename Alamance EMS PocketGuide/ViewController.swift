9e849592fbeaaf8298161e72ee12e6e2edba75d5
Ryan Rudinger <ryanrudinger@ryans-mbp.elon.edu>
2017-11-27 21:07:23 -0500

    Initial Commit

---
+++ Alamance EMS PocketGuide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alamance EMS PocketGuide
+//
+//  Created by Ryan Rudinger on 11/27/17.
+//  Copyright © 2017 Ryan Rudinger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
